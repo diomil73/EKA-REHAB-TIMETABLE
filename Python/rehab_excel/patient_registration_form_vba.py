77ee0140c1a00932a618a747e9c1a882b3633996
diomil73 <milidakisdionissis@gmail.com>
2026-09-26 19:44:59 +0300

    Update patient form for automatic ID and outpatient type

--- Python/rehab_excel/patient_registration_form_vba.py
+++ Python/rehab_excel/patient_registration_form_vba.py
@@ -7,28 +7,59 @@ PATIENT_FORM_CODE = '''Option Explicit
 Private Sub UserForm_Initialize()
     With Me
         .Caption = "Νέος ασθενής"
-        .Width = 430
-        .Height = 430
+        .Width = 500
+        .Height = 560
         .StartUpPosition = 1
         .BackColor = RGB(245, 247, 250)
     End With
 
-    StyleTitle lblTitle, "Εγγραφή νέου ασθενή", 20
-    StyleLabel lblPatientID, "Patient ID *", 64
-    StyleTextBox txtPatientID, 60
-    StyleLabel lblDisplayName, "Ονοματεπώνυμο *", 108
-    StyleTextBox txtDisplayName, 104
-    StyleLabel lblRoom, "Θάλαμος", 152
-    StyleComboBox cboRoom, 148
-    StyleLabel lblInfectious, "Λοιμώδης", 196
-    StyleCheckBox chkInfectious, "Ναι", 192
-    StyleLabel lblStatus, "Κατάσταση", 240
-    StyleComboBox cboStatus, 236
+    StyleTitle lblTitle, "Εγγραφή νέου ασθενή", 18
+
+    StyleLabel lblPatientType, "Τύπος ασθενή *", 62
+    StyleComboBox cboPatientType, 58
+    cboPatientType.AddItem "Εσωτερικός"
+    cboPatientType.AddItem "Εξωτερικός"
+    cboPatientType.ListIndex = 0
+
+    StyleLabel lblPatientID, "Patient ID", 106
+    StyleTextBox txtPatientID, 102
+    txtPatientID.Text = "Αυτόματο κατά την αποθήκευση"
+    txtPatientID.Locked = True
+    txtPatientID.TabStop = False
+    txtPatientID.BackColor = RGB(238, 242, 247)
+
+    StyleLabel lblHospitalMRN, "ΑΜ Νοσοκομείου", 150
+    StyleTextBox txtHospitalMRN, 146
+
+    StyleLabel lblDisplayName, "Ονοματεπώνυμο *", 194
+    StyleTextBox txtDisplayName, 190
+
+    StyleLabel lblRoom, "Θάλαμος", 238
+    StyleComboBox cboRoom, 234
+
+    StyleLabel lblInfectious, "Λοιμώδης", 282
+    StyleCheckBox chkInfectious, "Ναι", 278
+
+    StyleLabel lblStatus, "Κατάσταση", 326
+    StyleComboBox cboStatus, 322
+
+    With lblInfo
+        .Caption = "Το Patient ID είναι μόνιμος εσωτερικός κωδικός. Ο ΑΜ Νοσοκομείου μπορεί να συμπληρωθεί και αργότερα."
+        .Left = 38
+        .Top = 370
+        .Width = 420
+        .Height = 38
+        .WordWrap = True
+        .Font.Name = "Calibri"
+        .Font.Size = 9
+        .ForeColor = RGB(71, 85, 105)
+        .BackStyle = 0
+    End With
 
     With lblRequired
         .Caption = "* Υποχρεωτικό πεδίο"
-        .Left = 36
-        .Top = 286
+        .Left = 38
+        .Top = 414
         .Width = 180
         .Height = 18
         .Font.Name = "Calibri"
@@ -39,9 +70,9 @@ Private Sub UserForm_Initialize()
 
     With cmdSave
         .Caption = "Έλεγχος στοιχείων"
-        .Left = 178
-        .Top = 325
-        .Width = 150
+        .Left = 235
+        .Top = 452
+        .Width = 170
         .Height = 34
         .Font.Name = "Calibri"
         .Font.Size = 10
@@ -51,9 +82,9 @@ Private Sub UserForm_Initialize()
 
     With cmdCancel
         .Caption = "Ακύρωση"
-        .Left = 58
-        .Top = 325
-        .Width = 105
+        .Left = 92
+        .Top = 452
+        .Width = 125
         .Height = 34
         .Font.Name = "Calibri"
         .Font.Size = 10
@@ -61,15 +92,16 @@ Private Sub UserForm_Initialize()
     End With
 
     LoadSettingsValues
-    txtPatientID.SetFocus
+    ApplyPatientTypeRules
+    cboPatientType.SetFocus
 End Sub
 
 Private Sub StyleTitle(ByVal control As MSForms.Label, ByVal text As String, ByVal topPosition As Single)
     With control
         .Caption = text
-        .Left = 36
+        .Left = 38
         .Top = topPosition
-        .Width = 350
+        .Width = 420
         .Height = 26
         .TextAlign = 2
         .Font.Name = "Calibri"
@@ -83,9 +115,9 @@ End Sub
 Private Sub StyleLabel(ByVal control As MSForms.Label, ByVal text As String, ByVal topPosition As Single)
     With control
         .Caption = text
-        .Left = 36
+        .Left = 38
         .Top = topPosition
-        .Width = 120
+        .Width = 145
         .Height = 20
         .Font.Name = "Calibri"
         .Font.Size = 10
@@ -96,9 +128,9 @@ End Sub
 
 Private Sub StyleTextBox(ByVal control As MSForms.TextBox, ByVal topPosition As Single)
     With control
-        .Left = 165
+        .Left = 190
         .Top = topPosition
-        .Width = 220
+        .Width = 265
         .Height = 24
         .Font.Name = "Calibri"
         .Font.Size = 10
@@ -107,9 +139,9 @@ End Sub
 
 Private Sub StyleComboBox(ByVal control As MSForms.ComboBox, ByVal topPosition As Single)
     With control
-        .Left = 165
+        .Left = 190
         .Top = topPosition
-        .Width = 220
+        .Width = 265
         .Height = 24
         .Font.Name = "Calibri"
         .Font.Size = 10
@@ -120,7 +152,7 @@ End Sub
 Private Sub StyleCheckBox(ByVal control As MSForms.CheckBox, ByVal text As String, ByVal topPosition As Single)
     With control
         .Caption = text
-        .Left = 165
+        .Left = 190
         .Top = topPosition
         .Width = 80
         .Height = 24
@@ -158,10 +190,32 @@ SettingsError:
     MsgBox "Δεν ήταν δυνατή η φόρτωση των επιλογών από το φύλλο SETTINGS.", vbExclamation, "Νέος ασθενής"
 End Sub
 
+Private Sub cboPatientType_Change()
+    ApplyPatientTypeRules
+End Sub
+
+Private Sub ApplyPatientTypeRules()
+    Dim isInpatient As Boolean
+    isInpatient = (cboPatientType.Value <> "Εξωτερικός")
+
+    lblRoom.Enabled = isInpatient
+    cboRoom.Enabled = isInpatient
+    lblInfectious.Enabled = isInpatient
+    chkInfectious.Enabled = isInpatient
+    lblStatus.Enabled = isInpatient
+    cboStatus.Enabled = isInpatient
+
+    If Not isInpatient Then
+        cboRoom.ListIndex = -1
+        chkInfectious.Value = False
+        cboStatus.ListIndex = -1
+    End If
+End Sub
+
 Private Function ValidateForm() As Boolean
-    If Len(Trim$(txtPatientID.Text)) = 0 Then
-        MsgBox "Το Patient ID είναι υποχρεωτικό.", vbExclamation, "Έλεγχος στοιχείων"
-        txtPatientID.SetFocus
+    If Len(Trim$(cboPatientType.Value)) = 0 Then
+        MsgBox "Ο τύπος ασθενή είναι υποχρεωτικός.", vbExclamation, "Έλεγχος στοιχείων"
+        cboPatientType.SetFocus
         Exit Function
     End If
 
@@ -176,14 +230,26 @@ End Function
 
 Private Sub cmdSave_Click()
     Dim message As String
+    Dim patientType As String
 
     If Not ValidateForm() Then Exit Sub
 
-    message = "Patient ID: " & Trim$(txtPatientID.Text) & vbCrLf & _
-              "Ονοματεπώνυμο: " & Trim$(txtDisplayName.Text) & vbCrLf & _
-              "Θάλαμος: " & IIf(Len(cboRoom.Value) > 0, cboRoom.Value, "-") & vbCrLf & _
-              "Λοιμώδης: " & IIf(chkInfectious.Value, "Ναι", "Όχι") & vbCrLf & _
-              "Κατάσταση: " & IIf(Len(cboStatus.Value) > 0, cboStatus.Value, "-") & vbCrLf & vbCrLf & _
+    patientType = Trim$(cboPatientType.Value)
+    message = "Τύπος: " & patientType & vbCrLf & _
+              "Patient ID: Αυτόματο κατά την αποθήκευση" & vbCrLf & _
+              "ΑΜ Νοσοκομείου: " & IIf(Len(Trim$(txtHospitalMRN.Text)) > 0, Trim$(txtHospitalMRN.Text), "-") & vbCrLf & _
+              "Ονοματεπώνυμο: " & Trim$(txtDisplayName.Text) & vbCrLf
+
+    If patientType = "Εσωτερικός" Then
+        message = message & _
+                  "Θάλαμος: " & IIf(Len(cboRoom.Value) > 0, cboRoom.Value, "-") & vbCrLf & _
+                  "Λοιμώδης: " & IIf(chkInfectious.Value, "Ναι", "Όχι") & vbCrLf & _
+                  "Κατάσταση: " & IIf(Len(cboStatus.Value) > 0, cboStatus.Value, "-") & vbCrLf
+    Else
+        message = message & "Εξωτερικός ασθενής: δεν εμφανίζεται στα φύλλα νοσηλευομένων." & vbCrLf
+    End If
+
+    message = message & vbCrLf & _
               "Τα στοιχεία είναι έτοιμα για αποστολή στο ασφαλές registration backend."
 
     MsgBox message, vbInformation, "Έλεγχος νέου ασθενή"
@@ -215,20 +281,25 @@ def install_patient_form(vbproject, *, position_control) -> None:
     designer.Caption = "Νέος ασθενής"
 
     controls = (
-        ("Forms.Label.1", "lblTitle", "Εγγραφή νέου ασθενή", 20),
-        ("Forms.Label.1", "lblPatientID", "Patient ID *", 64),
-        ("Forms.TextBox.1", "txtPatientID", None, 60),
-        ("Forms.Label.1", "lblDisplayName", "Ονοματεπώνυμο *", 108),
-        ("Forms.TextBox.1", "txtDisplayName", None, 104),
-        ("Forms.Label.1", "lblRoom", "Θάλαμος", 152),
-        ("Forms.ComboBox.1", "cboRoom", None, 148),
-        ("Forms.Label.1", "lblInfectious", "Λοιμώδης", 196),
-        ("Forms.CheckBox.1", "chkInfectious", "Ναι", 192),
-        ("Forms.Label.1", "lblStatus", "Κατάσταση", 240),
-        ("Forms.ComboBox.1", "cboStatus", None, 236),
-        ("Forms.Label.1", "lblRequired", "* Υποχρεωτικό πεδίο", 286),
-        ("Forms.CommandButton.1", "cmdCancel", "Ακύρωση", 325),
-        ("Forms.CommandButton.1", "cmdSave", "Έλεγχος στοιχείων", 325),
+        ("Forms.Label.1", "lblTitle", "Εγγραφή νέου ασθενή", 18),
+        ("Forms.Label.1", "lblPatientType", "Τύπος ασθενή *", 62),
+        ("Forms.ComboBox.1", "cboPatientType", None, 58),
+        ("Forms.Label.1", "lblPatientID", "Patient ID", 106),
+        ("Forms.TextBox.1", "txtPatientID", None, 102),
+        ("Forms.Label.1", "lblHospitalMRN", "ΑΜ Νοσοκομείου", 150),
+        ("Forms.TextBox.1", "txtHospitalMRN", None, 146),
+        ("Forms.Label.1", "lblDisplayName", "Ονοματεπώνυμο *", 194),
+        ("Forms.TextBox.1", "txtDisplayName", None, 190),
+        ("Forms.Label.1", "lblRoom", "Θάλαμος", 238),
+        ("Forms.ComboBox.1", "cboRoom", None, 234),
+        ("Forms.Label.1", "lblInfectious", "Λοιμώδης", 282),
+        ("Forms.CheckBox.1", "chkInfectious", "Ναι", 278),
+        ("Forms.Label.1", "lblStatus", "Κατάσταση", 326),
+        ("Forms.ComboBox.1", "cboStatus", None, 322),
+        ("Forms.Label.1", "lblInfo", "", 370),
+        ("Forms.Label.1", "lblRequired", "* Υποχρεωτικό πεδίο", 414),
+        ("Forms.CommandButton.1", "cmdCancel", "Ακύρωση", 452),
+        ("Forms.CommandButton.1", "cmdSave", "Έλεγχος στοιχείων", 452),
     )
 
     for prog_id, name, caption, top in controls:
